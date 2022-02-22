54ec69a85b558fa51cf5f8040c26755a8adc082a
Philipp Streit <philipp.streit@edu.hefr.ch>
2022-02-22 11:44:25 +0100

    add: Maven init project

---
+++ src/main/java/ch/hefr/iscrsid/gl1/strmauroc/App.java
@@ -0,0 +1,13 @@
+package ch.hefr.iscrsid.gl1.strmauroc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
